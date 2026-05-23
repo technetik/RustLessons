ce8f50f2e971f5edf2e7c00370cf0fecf0409430
technetik <id@technetik.ru>
2026-05-23 23:05:09 +0300

    update

--- lesson2/src/main.rs
+++ lesson2/src/main.rs
@@ -1,3 +1,13 @@
 fn main() {
-    println!("Hello, world!");
+    
+    //Затенения  (shadowing) переменных
+    //по сути затенение переменной(сокрытие переменной), это ее новое объявление
+    let test = "11.11";
+    let test = 11.11;
+    let mut test = 11;
+    test = 111;
+
+    println!("Hello, world! {}", test);
+
+
 }
