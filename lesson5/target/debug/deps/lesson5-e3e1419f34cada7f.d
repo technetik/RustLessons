938ee0013d3326ada53d4427364bbee3dff14246
technetik <id@technetik.ru>
2026-05-25 21:14:31 +0300

    Delete lesson5/target directory
    del

--- lesson5/target/debug/deps/lesson5-e3e1419f34cada7f.d
+++
@@ -1,5 +0,0 @@
-/Users/technetik/code/RustLessons/lesson5/target/debug/deps/lesson5-e3e1419f34cada7f.d: src/main.rs
-
-/Users/technetik/code/RustLessons/lesson5/target/debug/deps/lesson5-e3e1419f34cada7f: src/main.rs
-
-src/main.rs:
--- lesson5/target/debug/lesson5.d
+++
@@ -1,1 +0,0 @@
-/Users/technetik/code/RustLessons/lesson5/target/debug/lesson5: /Users/technetik/code/RustLessons/lesson5/src/main.rs
