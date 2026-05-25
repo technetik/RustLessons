eb445875eba7e295cac5a282ffd6f6f9c0212ac0
technetik <id@technetik.ru>
2026-05-25 19:46:54 +0300

    Update

---
+++ lesson4/target/debug/deps/lesson4-06585cd8c465acad.d
@@ -0,0 +1,5 @@
+/Users/technetik/code/RustLessons/lesson4/target/debug/deps/lesson4-06585cd8c465acad.d: src/main.rs
+
+/Users/technetik/code/RustLessons/lesson4/target/debug/deps/liblesson4-06585cd8c465acad.rmeta: src/main.rs
+
+src/main.rs:
