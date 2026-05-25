31c3e166f68ccb13faf1e5e199479d9b6cc23b4a
technetik <id@technetik.ru>
2026-05-25 10:46:13 +0300

    Update

---
+++ lesson3/target/debug/deps/lesson3-bb6a8437d01202ab.d
@@ -0,0 +1,5 @@
+/Users/technetik/code/RustLessons/lesson3/target/debug/deps/lesson3-bb6a8437d01202ab.d: src/main.rs
+
+/Users/technetik/code/RustLessons/lesson3/target/debug/deps/lesson3-bb6a8437d01202ab: src/main.rs
+
+src/main.rs:
---
+++ lesson3/target/debug/lesson3.d
@@ -0,0 +1,1 @@
+/Users/technetik/code/RustLessons/lesson3/target/debug/lesson3: /Users/technetik/code/RustLessons/lesson3/src/main.rs
