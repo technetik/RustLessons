323665311d96edc332f7d211d70a22e67afd892c
technetik <id@technetik.ru>
2026-05-25 21:05:08 +0300

    Delete lesson1/target directory

--- lesson1/target/debug/deps/lesson1-058f963ab7187bfb.d
+++
@@ -1,5 +0,0 @@
-/Users/technetik/code/RustLessons/lesson1/target/debug/deps/lesson1-058f963ab7187bfb.d: src/main.rs
-
-/Users/technetik/code/RustLessons/lesson1/target/debug/deps/liblesson1-058f963ab7187bfb.rmeta: src/main.rs
-
-src/main.rs:
--- lesson1/target/debug/deps/lesson1-522454a571738dc8.d
+++
@@ -1,5 +0,0 @@
-/Users/technetik/code/RustLessons/lesson1/target/debug/deps/lesson1-522454a571738dc8.d: src/main.rs
-
-/Users/technetik/code/RustLessons/lesson1/target/debug/deps/lesson1-522454a571738dc8: src/main.rs
-
-src/main.rs:
