ee3c7617f9541b0bb5d07a4aa6062065b4701592
technetik <id@technetik.ru>
2026-06-01 01:05:06 +0300

    Up

--- lesson7/lesson8/target/debug/deps/lesson8-6a01ff523fe4da18.d
+++
@@ -1,5 +0,0 @@
-/Users/technetik/code/RustLessons/lesson7/lesson8/target/debug/deps/lesson8-6a01ff523fe4da18.d: src/main.rs
-
-/Users/technetik/code/RustLessons/lesson7/lesson8/target/debug/deps/lesson8-6a01ff523fe4da18: src/main.rs
-
-src/main.rs:
--- lesson7/lesson8/target/debug/lesson8.d
+++
@@ -1,1 +0,0 @@
-/Users/technetik/code/RustLessons/lesson7/lesson8/target/debug/lesson8: /Users/technetik/code/RustLessons/lesson7/lesson8/src/main.rs
