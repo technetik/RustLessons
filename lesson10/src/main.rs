0aa2266aba582c3a035f24f1c1802a870ba4236c
technetik <id@technetik.ru>
2026-06-12 22:47:18 +0300

    Up

--- lesson10/src/main.rs
+++ lesson10/src/main.rs
@@ -1,6 +1,15 @@
 fn main() {
-    // Strings and Raw Strings
+    // Strings &str and Raw Strings &str
     let _path ="C:\\Users\\name\\documents"; // экранируем слэши или пример ниже 
     let path = r"C:\Users\name\documents"; // слэши остаются обычными слэшами
     println!("{path}");
+
+    // Тип String
+    let text1 = String::new();
+    let text2 = String::from("Просто текст");
+
+    let mut text3 = String::from("Просто текст");
+    println!("{text3}");
+    text3.push_str(" еще текст");
+    println!("{text3}");
 }
--- lesson15/src/main.rs
+++ lesson15/src/main.rs
@@ -1,7 +1,8 @@
 // Recursion
 fn countup(sec: i32) {
+    // base case условие которое останавливает рекурсию
     if sec == 0 {
-        println!("Стоп");
+        println!("Стоп рекурсия"); 
     } else {
         println!("{sec} обратный отсчет ");
         countup(sec-1);
