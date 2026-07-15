41b7729cc25971fa76ea655a48207063b7128f15
technetik <id@technetik.ru>
2026-07-15 20:41:53 +0300

    Up

--- lesson27/src/main.rs
+++ lesson27/src/main.rs
@@ -1,3 +1,45 @@
+#[derive(Debug)]
+struct Users {
+    name: String,
+    age: u32,
+    year: u32,
+}
+
+// Варианты создания метода
+// fn print_user(self)
+// fn print_user(self: Users) 
+// fn print_user(self: Self)
+
+impl Users{
+    /*
+    Чтобы объект можно было использовать многократно, в аргументах методов 
+    обычно используют ссылки: fn print_user(&self) и fn year_plus(&mut self).
+     */
+    fn print_user(self){
+        println!("Имя: {}", self.name);
+        println!("Возраст: {}", self.age);
+        println!("Год: {}", self.year);
+    }
+    fn year_plus(mut self){
+        self.year = self.year + 1;
+        println!("{:#?}", self);
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let user1 = Users{
+        name: String::from("Вася"),
+        age: 32,
+        year: 1991,
+    };
+    // Объект user1 "умер" здесь, так как print_user забрал владение.
+    user1.print_user();
+
+        let user2 = Users{
+        name: String::from("Петя"),
+        age: 29,
+        year: 1991,
+    };
+    // Объект user2 "умер" здесь, так как year_plus забрал владение.
+    user2.year_plus();
 }
